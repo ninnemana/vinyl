26f333d21b26dcc096056bdcb68f860ef06a1d66
Alex Ninneman <alex@ninneman.org>
2020-02-02 10:10:46 -0600

    use drudge forward stream to disable chunking

--- pkg/vinyl/errors.go
+++ pkg/vinyl/errors.go
@@ -1,8 +1,15 @@
 package vinyl
 
-import "github.com/pkg/errors"
+import (
+	"github.com/ninnemana/drudge"
+	"github.com/pkg/errors"
+)
 
 var (
 	ErrNotFound         = errors.Errorf("no vinyl was found")
 	ErrInvalidGetParams = errors.New("invalid parameters supplied when attempting to retrieve a vinyl")
 )
+
+func init() {
+	forward_Vinyl_Search_0 = drudge.ForwardResponseStream
+}
