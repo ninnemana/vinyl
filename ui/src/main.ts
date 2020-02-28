c67782b309d9164d5e829dda889b032dfa43c7ad
Alex Ninneman <alex@ninneman.org>
2020-02-28 16:29:42 -0600

    vuetify

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
 
 Vue.use(Vuetify);
 
@@ -14,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -1,16 +1,56 @@
 <template>
-    <div class='login'>
-        <h1>Login</h1>
-        <md-field md-inline>
-            <label>Email Address</label>
-            <md-input></md-input>
-        </md-field>
-    </div>
+  <v-content>
+    <v-container fluid>
+      <v-row>
+        <v-col class='login' cols='8'>
+          <v-row :align='alignment' :justify='justify'>
+            <v-card class='elevation-12'>
+              <v-card-text>
+                <v-form ref='form' v-model='valid' :lazy-validation='lazy'>
+                  <v-text-field label='Login' name='login' prepend-icon='person' type='text' />
+
+                  <v-text-field
+                    id='password'
+                    label='Password'
+                    name='password'
+                    prepend-icon='lock'
+                    type='password'
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color='primary' class>Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
 export default {
-    name: 'Login',
-    components: {}
-}
+  name: 'Login',
+  components: {},
+  props: {
+      valid: {},
+  }
+};
 </script>
+
+<style scoped lang='scss'>
+.login {
+  margin: 0 auto;
+  flex: 1;
+
+  .elevation-12 {
+    flex: 1;
+
+    .primary.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+      background-color: #4db6ac;
+    }
+  }
+}
+</style>
